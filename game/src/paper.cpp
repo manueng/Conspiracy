aae04dacd18f19da7f1cc609c7435d7a5761d0c4
Emanuel <manusenac@hotmail.com>
2017-08-23 08:36:56 -0300

    Update paper.cpp
    mudando somente devido ao encademento da classe aliens.bidu.

--- game/src/paper.cpp
+++ game/src/paper.cpp
@@ -12,17 +12,17 @@ Paper::Paper(std::string objectName, double positionX, double positionY,
     std::vector<unsigned int> frontColor = {116, 225, 117, 1};
     editing_bar = new ProgressBar(positionX-3, positionY-8, 22.5, 5, 0.005, backColor, frontColor);
 
-    paperEditingSound = new Audio("assets/sounds/PAPEROISE.wav", "EFFECT", 100);
+    paper_Editing_Sound = new Audio("assets/sounds/PAPEROISE.wav", "EFFECT", 100);
 
-    animator->addAction("idle",0,0);
-    animator->addAction("beingEdited",1,3);
+    Animator->add_Action("idle",0,0);
+    Animator->addAction("beingEdited",1,3);
     edited = false;
     isBeingEdited = false;
 }
 
 Paper::~Paper(){}
 
-void Paper::update(double timeElapsed){
+void Paper::Update(double timeElapsed){
     timeElapsed = timeElapsed;
     if(isBeingEdited){
         editing_bar->update(timeElapsed);
@@ -37,19 +37,19 @@ void Paper::update(double timeElapsed){
     animator->update();
 }
 
-void Paper::animate(){
+void Paper::Animate(){
     isBeingEdited = true;
 }
 
-void Paper::stopAnimation(){
+void Paper::Stop_Animation(){
     isBeingEdited = false;
 }
 
-bool Paper::isEdited(){
+bool Paper::Is_Edited(){
     return edited;
 }
 
-void Paper::draw(){
+void Paper::Draw(){
     INFO("Paper DRAW");
     animator->draw(getPositionX(), getPositionY());
     animator->draw_collider(getPositionX(), getPositionY(), getWidth(), getHeight());
@@ -62,18 +62,18 @@ Animation * Paper::getAnimation(){
   return animator;
 }
 
-double Paper::getEditingBarPercent(){
+double Paper::Get_Editing_Bar_Percent(){
     return editing_bar->getPercent();
 }
 
-void Paper::resetEditingProgress(){
+void Paper::Reset_Editing_Progress(){
     editing_bar->resetPercent();
 }
 
-void Paper::playEffect(){
+void Paper::Play_Effect(){
     paperEditingSound->play(0);
 }
 
-void Paper::stopEffect(){
+void Paper::Stop_Effect(){
     paperEditingSound->stop();
 }
